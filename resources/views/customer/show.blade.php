659898e5d5227b8604b2ebeb567fab6250f24171
NEVAR <danielo-24@hotmail.com>
2026-09-08 15:23:30 +0200

    fixed show

--- resources/views/customer/show.blade.php
+++ resources/views/customer/show.blade.php
@@ -1,5 +1,6 @@
 <x-layout.base title="Customer {{ $customer->label }}">
-    <p><a href="{{ route('customer.index') }}">back to list</a></p>
+    <h1>Client n° {{ $customer->id }}</h1>
+    <p><a href="{{ route('customer.index') }}">Retour à la liste des clients</a></p>
     <h1>{{ $customer->label }}</h1>
     <p>{{ $customer->email }}</p>
     <p>{{ $customer->firstname }} {{ $customer->lastname }}</p>
