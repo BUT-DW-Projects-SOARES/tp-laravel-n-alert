0883b7bc5dcc575f8ae3291d72bbca09bced2e8a
NEVAR <danielo-24@hotmail.com>
2026-09-08 15:21:38 +0200

    alert published date as carbon instance

--- resources/views/alert/show.blade.php
+++ resources/views/alert/show.blade.php
@@ -3,6 +3,6 @@
     <p><a href="{{ route('home.index') }}">Voir l'accueil</a></p>
     <p><a href="{{ route('alert.index') }}">Voir les alertes</a></p>
     <h1>{{ $alert->title }}</h1>
-    <p>{{ $alert->published_at }}</p>
+    <p>{{ $alert->published_at->isoFormat('L HH:mm') }}</p>
     <p>{{ $alert->description }}</p>
 </x-layout.base>
