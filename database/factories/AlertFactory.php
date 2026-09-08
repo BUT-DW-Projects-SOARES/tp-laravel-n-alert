679327a7018a1472504f6ae136bb9c520a9d4c1e
NEVAR <danielo-24@hotmail.com>
2026-09-08 10:21:30 +0200

    factory added

---
+++ database/factories/AlertFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Alert;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Alert>
+ */
+class AlertFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'published_at' => fake()->dateTimeThisMonth(),
+        ];
+    }
+}
