67cbe591e86e952fc18ce1ce26ca963f12fa2a6b
NEVAR <danielo-24@hotmail.com>
2026-09-08 09:39:19 +0200

    add route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/hello/{name}', function ($name) {
+    return $name;
+});
